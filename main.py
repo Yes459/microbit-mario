d93c17e0e116ab7f0a2ddef92a08ea45c8456bc2
Yes459 <77157796+Yes459@users.noreply.github.com>
2021-01-11 08:55:35 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,36 +2,51 @@ def on_forever():
     global x
     global y
     global world
+    global worldW
 
     basic.clear_screen()
-    led.plot_brightness(2, y, 255)
+    t = 0
+    if(world[y + 1][x] == 0):
+        y += 1
+    if(x < 2):
+        t = 2 - x
+    elif(x > worldW - 3):
+        t = worldW - 3 - x
+    led.plot_brightness(2 - t, y, 255)
     for wx in range(5):
         for wy in range(5):
-            if(world[wy][wx + x - 2] == 1):
+            if(world[wy][wx + x - 2 + t] == 1):
                 led.plot_brightness(wx, wy, 100)
+    basic.pause(250)
 basic.forever(on_forever)
 
 def on_button_pressed_a():
     global x
-    x -= 1
+    global world
+    if(x > 0 and world[y][x - 1] == 0):
+        x -= 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global x
-    x += 1
+    global worldW
+    global world
+    if(x < worldW - 1 and world[y][x + 1] == 0):
+        x += 1
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_button_pressed_ab():
     global y
-    y -= 2
+    y = -1
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 world = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
+worldW = len(world[0])
 x = 2
 y = 2
